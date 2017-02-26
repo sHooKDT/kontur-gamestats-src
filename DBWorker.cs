02582100ddc8034ac796d2b5fe9b505bcc54c3ff
Vladimir Koch <vladimirkoch@gmx.de>
2017-02-26 16:49:33 +0100

    DBWorker interface extended: GetServersInfo(), GetServerInfo(string endpoint), PutServerInfo(EndpointInfo server);

--- DBWorker.cs
+++ DBWorker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
 using Kontur.GameStats.Server.Datatypes;
 
@@ -30,34 +31,57 @@ namespace Kontur.GameStats.Server
 	        sqlCommand.ExecuteNonQuery();
 	    }
 
-	    public int PutServerInfo(string endpoint, string name, string gamemodes)
-	    {
-	        sqlCommand.CommandText =
-	            $@"INSERT OR REPLACE INTO servers VALUES (""{endpoint}"", ""{name}"", ""{gamemodes}"")";
-
-	        return sqlCommand.ExecuteNonQuery();
-	    }
-
         public EndpointInfo[] GetServersInfo()
         {
-            sqlCommand.CommandText = "SELECT * FROM server";
+            List<EndpointInfo> servers = new List<EndpointInfo>();
+
+            sqlCommand.CommandText = "SELECT * FROM servers";
             SQLiteDataReader reader = sqlCommand.ExecuteReader();
             while(reader.Read())
             {
+                string endpoint = (string)reader["endpoint"];
+                string name = (string)reader["name"];
+                string gamemodes = (string)reader["gamemodes"];
 
+                servers.Add(new EndpointInfo(endpoint, name, gamemodes));
             }
 
-            throw new NotImplementedException();
+            return servers.ToArray();
         }
 
         public EndpointInfo.ServerInfo GetServerInfo(string endpoint)
         {
-            throw new NotImplementedException();
+            EndpointInfo.ServerInfo serverInfo = null;
+
+            sqlCommand.CommandText = String.Format("SELECT name, gamemodes FROM servers WHERE endpoint = {0}", endpoint);
+            SQLiteDataReader reader = sqlCommand.ExecuteReader();
+            while(reader.Read())
+            {
+                serverInfo = new EndpointInfo.ServerInfo(reader.GetString(0), reader.GetString(1);
+                break;
+            }
+
+            return serverInfo;
         }
 
         public bool PutServerInfo(EndpointInfo server)
         {
-            throw new NotImplementedException();
+            bool result = false;
+
+            sqlCommand.CommandText = String.Format(@"INSERT OR REPLACE INTO servers VALUES (""{0}"", ""{1}"", ""{2}"")",
+                server.GetEndpoint(),
+                server.GetName(),
+                server.GetGameModes()
+                );
+
+            int rows = -1;
+            rows = sqlCommand.ExecuteNonQuery();
+            if(rows >= 0)
+            {
+                result = true;
+            }
+
+            return result;
         }
 
         public MatchInfo GetServerMatch(string endpoint, string timestamp)
--- Datatypes/EndpointInfo.cs
+++ Datatypes/EndpointInfo.cs
@@ -2,13 +2,60 @@
 {
     public class EndpointInfo
     {
-        public string endpoint;
-        public ServerInfo info;
+        private string endpoint;
+        private ServerInfo info;
 
         public class ServerInfo
         {
-            public string name;
-            public string[] gameModes;
+            private string name;
+            private string[] gameModes;
+
+            public ServerInfo(string name, string gamemodes)
+            {
+                this.name = name;
+                gameModes = gamemodes.Split(',');
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public string[] GetGameModes()
+            {
+                return gameModes;
+            }
+        }
+
+        public EndpointInfo(string endpoint, string name, string gamemodes)
+        {
+            this.endpoint = endpoint;
+            info = new ServerInfo(name, gamemodes);
+        }
+
+        public string GetEndpoint()
+        {
+            return endpoint;
+        }
+
+        public string GetName()
+        {
+            return info.GetName();
+        }
+
+        public string GetGameModes()
+        {
+            string result = "";
+            int N = info.GetGameModes().Length;
+            for (int i = 0; i < N; i++)
+            {
+                result += info.GetGameModes()[i];
+                if(i != N - 1)
+                {
+                    result += ",";
+                }
+            }
+            return result;
         }
     }
 }
